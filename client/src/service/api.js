b923fb8e30bb0b2a8e18ee344550b8dcccac5151
Shivam Malviya <86865439+shivammalviya205@users.noreply.github.com>
2022-02-19 12:32:12 +0530

    changes made to url

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const url = 'http://localhost:5000';
+const url = '';
 export const createPost = async (post) => {
     try {
         return await axios.post(`${url}/post/create`, post);
