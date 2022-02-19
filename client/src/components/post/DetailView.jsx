48d4c75c407dc8f62730b31dd2d4b732dfb00090
Shivam Malviya <86865439+shivammalviya205@users.noreply.github.com>
2022-02-19 13:39:08 +0530

    Update DetailView.jsx

--- client/src/components/post/DetailView.jsx
+++ client/src/components/post/DetailView.jsx
@@ -71,7 +71,7 @@ fetchData();
     const deleted=async()=>{
         
          await deletePost(id)
-        navigate('/')
+        navigate('/home')
     }
     const url= post.picture ? post.picture : 'https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80'
     return(
