bac0ebb7bee2a599fa0f330200e224ae9493df71
Shivam Malviya <86865439+shivammalviya205@users.noreply.github.com>
2022-02-19 13:25:56 +0530

    Update CreateView.jsx

--- client/src/components/post/CreateView.jsx
+++ client/src/components/post/CreateView.jsx
@@ -61,7 +61,7 @@ const initialCategory={
 function CreateView() {
     const classes=useStyle();
      
-    window.location.reload();
+    
  
     const url='https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80'
      const navigate=useNavigate();
